f0b6d57947cf97f04c9e59635157f829b303cdfd
Mascot-one <shubhankar.0727@gmail.com>
2025-07-12 13:56:08 -0700

    1

--- TrappingW.java
+++ TrappingW.java
@@ -3,7 +3,17 @@ import java.util.*;
 public class JavaBasics {                            
 
     public static void main(String args[]) {              
+    Scanner sc = new Scanner(System.in);
     
+    int height[] = {4,2,0,6,3,2,5};
+
+    trap(height);
+    }
+
+    public static void trap(int height[]){
+        
+        int maxR = integer.MIN_VALUE;
+        int maxL = integer.MIN_VALUE;
     
-}
+    }
 }
