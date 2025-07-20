9cc971551f52097bd1a5aee2de1c91337463c1bd
Mascot-one <shubhankar.0727@gmail.com>
2025-07-20 11:39:24 -0700

    more optimized big O(n)

--- TrappingW.java
+++ TrappingW.java
@@ -1,56 +1,49 @@
 import java.util.*;
 
-public class TrappingW {                            
+public class JavaBasics{
 
-    public static void main(String args[]) {              
-    Scanner sc = new Scanner(System.in);
-    
-    int height[] = {4,2,0,6,3,2,5};
-    int width = 1;
+    public static void main(String args[]){
 
-    trap(height,width);
+        int height[] = {4, 2, 0, 3, 2, 5};
+        int width = 1;
+
+        System.out.print(trap(height,width));
     }
 
-    public static void trap(int height[], int width){
-        
-        int maxR = Integer.MIN_VALUE;
-        int maxL = Integer.MIN_VALUE;
-        int waterLvl = 0;
-        int trapWtr = 0;
+    public static int trap(int height[] , int width){
+        int n = height.length;
+        int Rmax[] = new int[n];
+        int Lmax[] = new int[n];
+        int Wlevel = 0;
+        int twater = 0;
 
-        for(int i = 0 ; i < height.length ; i++){
+            //Calculate Right max elements and store in array
+            Rmax [n-1] = height [n-1];
+            for(int r = n-2 ; r >= 0 ; r--){
 
-            for(int j = i ; j < height.length ; j++){      //maxRight
-
-                if (height[j] > maxR){
-                    maxR = height[j];
-                }
+                Rmax[r] = Math.max(height[r],Rmax[r+1]);
             }
 
-            for(int k = i ; k >= 0 ; k--){                 //maxLeft
+            //Calculate Left max elements and store in array
+            Lmax [0] = height[0];
+            for(int l = 1 ; l < n ; l++){
+
+                Lmax[l] = Math.max(height[l],Lmax[l-1]);
+
+            }
+
+
+            //find minimum between Rmax & Lmax, then find if it is greater than height[i] then update total traped water 
+            for(int i = 0 ; i < n ; i++){
                 
-                if (height[k] > maxL){
-                    maxL = height[k]; 
-                }    
-            }
+                Wlevel = Math.min(Rmax[i],Lmax[i]);
 
-            
-            if (maxR > maxL){                              //Take small one between maxL & maxR to find water level
-                waterLvl = maxL;
-            }else{
-                waterLvl = maxR;
-            }
+                if ( Wlevel > height[i]){
 
-            
-            if (waterLvl > height[i]){                     //if water level is more than height then only we can find trapped water level of perticuler block(if not then no change in trapWtr)  
-               trapWtr = trapWtr + (waterLvl - height[i])*width ; //formula to calculet total trapped water [Area = (water level - height of block) * Width of block]
-            }
+                    twater = twater + (Wlevel - height[i])*width;
+                }
 
-            maxR = Integer.MIN_VALUE;                   //Adjust the main variables at original position after using it because there is stores some values when we used it 
-            maxL = Integer.MIN_VALUE;
-            waterLvl = 0;
-        }   
-        
-        System.out.print("Trapped Rain water =  "+ trapWtr);
-    }
+           }
+           return twater;
+}
 }
